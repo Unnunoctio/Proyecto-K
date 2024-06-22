491a79ad29e32e5a222bfb7a88894c8f7f7e3fef
Rafael Carvacho <110637459+Unnunoctio@users.noreply.github.com>
2024-06-22 09:52:23 -0400

    refactor: :zap: changed qr image to qr buffer

--- src/utils/qr_code.py
+++ src/utils/qr_code.py
@@ -1,11 +1,18 @@
 import qrcode
+from io import BytesIO
 
-def generate_qr(text: str, path='qr_code.png'):
+def generate_qr(code: str):
   # TODO: crear QR code
   qr = qrcode.QRCode(version=1, box_size=10, border=1)
-  qr.add_data(text)
+  qr.add_data(code)
   qr.make(fit=True)
 
   # TODO: crear imagen del QR code
   img = qr.make_image(fill='black', back_color='white')
-  img.save(path)
+  
+  # TODO: guardar la imagen en un buffer
+  img_buffer = BytesIO()
+  img.save(img_buffer, format='PNG')
+  img_buffer.seek(0)
+
+  return img_buffer
